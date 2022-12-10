36b25e9cba0c947422272fea5aeab4980250ace7
zdj <zengdejin@zuoshouyisheng.com>
2022-12-10 23:39:43 +0800

    feat: update

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,58 +1,123 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
-  </div>
+  <el-table :data="tableData" style="wpbiIdth: 100%">
+    <el-table-column label="序号" wpbiIdth="180">
+      <template slot-scope="scope">
+        <p>{{ scope.row.order }}</p>
+      </template>
+    </el-table-column>
+    <el-table-column label="类型" wpbiIdth="180">
+      <template slot-scope="scope">
+        <div v-if="!activeEditIndex.includes(scope.$index)">
+          <span class="select-tags" closable v-for="(type, index) of scope.row.typeData" :key="index">{{ type }}</span>
+        </div>
+        <div v-if="activeEditIndex.includes(scope.$index)">
+          <treeselect 
+            :multiple="true" 
+            :options="scope.row.treeData" 
+            :default-expand-level="1" 
+            :flat="true" 
+            zIndex="9999"
+            placeholder="请输入" 
+            v-model="scope.row.typeData" 
+            :append-to-body="true" />
+        </div>
+      </template>
+    </el-table-column>
+    <el-table-column label="操作">
+      <template slot-scope="scope">
+        <el-button size="mini" @click="handleEdit(scope.$index, scope.row)"
+          v-if="!activeEditIndex.includes(scope.$index)">编辑</el-button>
+        <el-button size="mini" @click="handleSave(scope.$index, scope.row)"
+          v-if="activeEditIndex.includes(scope.$index)">保存</el-button>
+      </template>
+    </el-table-column>
+  </el-table>
 </template>
 
 <script>
+import Treeselect from '@riophae/vue-treeselect'
+// import the styles
+import '@riophae/vue-treeselect/dist/vue-treeselect.css'
 export default {
-  name: 'HelloWorld',
-  props: {
-    msg: String
+  data() {
+    return {
+      activeEditIndex: [],
+      tableData: [{
+        order: '1',
+        typeData: ['一级 1', '二级 1-1'],
+      }],
+      treeData: [{
+        pbiId: 1,
+        cn: '一级 1',
+        children: [{
+          pbiId: 2,
+          cn: '二级 1-1',
+          children: [{
+            pbiId: 3,
+            cn: '三级 1-1-1'
+          }, {
+            pbiId: 4,
+            cn: '三级 1-1-2'
+          }]
+        }]
+      }, {
+        pbiId: 5,
+        cn: '一级 2',
+        children: [{
+          pbiId: 6,
+          cn: '二级 2-1'
+        }, {
+          pbiId: 7,
+          cn: '二级 2-2'
+        }]
+      }],
+    }
+  },
+
+  components: {
+    Treeselect
+  },
+
+  created() {
+    this.deepSelectDataHandle()
+  },
+
+  methods: {
+    handleEdit(index, row) {
+      console.log('row:', row)
+      this.activeEditIndex.push(index);
+    },
+    handleSave(index, row) {
+      console.log('row:', row)
+      this.activeEditIndex = this.activeEditIndex.filter(item => item !== index);
+    },
+    deepSelectData(treeData) {
+      if (!treeData || !treeData.length) return [];
+      let data = [];
+      treeData.forEach(item => {
+        data.push({
+          id: item.cn,
+          label: item.cn,
+          children: item.children ? this.deepSelectData(item.children) : []
+        })
+      })
+      return data;
+    },
+    deepSelectDataHandle() {
+      this.tableData.forEach(item => {
+        item.treeData = this.deepSelectData(this.treeData);
+        console.log('item.treeData:', item.treeData)
+      })
+    },
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h3 {
-  margin: 40px 0 0;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
+<style>
+.select-tags {
+  padding: 10px;
+  background: #E6F2FC;
+  color: #5FA7E4;
+  border-radius: 15px;
+  margin-right: 10px;
 }
 </style>
